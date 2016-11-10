ebdba00de2d62b7f0ed64c43506b069e3344b4f9
Arturo9402 <arturooctavio37@gmail.com>
2016-11-10 13:59:02 -0600

    agragado recietes

--- alumnos/datos/models.py
+++ alumnos/datos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class InfoGeneral(models.Model):
+    nombre = models.CharField(max_length=50,
+                default="juanito")
+    apellido = models.CharField(max_length=50,
+         default ="")
+    telefono = models.CharField(max_length=20,
+        )
+    fecha_nacimiento = models.DateField()
+    semetre = models.IntegerField()
+    carrera = models.CharField(max_length=50)
+    sexo_options = ( 
+        ('f' , 'femenino',),
+        ('m' , 'masculino'),
+        )
+    sexo = models.CharField(max_length=2,
+            choices= sexo_options)
+    direccion = models.CharField(max_length=100)
